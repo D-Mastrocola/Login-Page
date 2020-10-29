89a949be70e37546f77aed7156a1ba99308fc8e3
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2020-10-28 20:41:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let emailRegex
+
+function init() {
+    emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+}
+
+
+
+
+function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  console.log(email);
+  console.log(password);
+  if (email.match(emailRegex)) {
+    console.log("email is valid");
+    if (password.length >= 8) {
+        redirect();
+    }
+  } else {
+    console.log("email is not valid");
+  }
+}
+function redirect() {
+    location.replace("/login/login.html");
+}
+
+
+init();
